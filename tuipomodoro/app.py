77a1d1c89bb61f8eea0fba597dd38ff4eae79e05
Nikita Timoshenko <nikita.timoshenko.dev@gmail.com>
2026-07-08 20:14:43 +0300

    initial commit. working simple timer countdown logic

---
+++ tuipomodoro/app.py
@@ -0,0 +1,58 @@
+from math import ceil
+from time import monotonic
+
+from textual.app import App, ComposeResult
+from textual.containers import VerticalGroup
+from textual.reactive import reactive
+from textual.widget import Widget
+from textual.widgets import Digits, Footer, Header, Static
+
+from tuipomodoro.timer import PomodoroTimer
+from tuipomodoro.utils import format_time
+
+
+class TimerDisplay(Widget):
+    """A widget to display remaining time"""
+
+    remaining = reactive(0.0)
+
+    def __init__(self, timer: PomodoroTimer, **kwargs):
+        super().__init__(**kwargs)
+        self.timer = timer
+
+    def watch_remaining(self, old: float, new: float) -> None:
+        self.query_one("#time", Digits).update(format_time(new))
+
+    def on_mount(self) -> None:
+        self.remaining = self.timer.duration
+        now = monotonic()
+        self.set_timer(ceil(now) - now, self._start_interval)
+
+    def _start_interval(self) -> None:
+        self.update_time()
+        self.set_interval(1, self.update_time)
+
+    def update_time(self) -> None:
+        self.remaining = self.timer.get_remaining()
+
+    def compose(self) -> ComposeResult:
+        yield Digits(id="time")
+        yield Static(id="progress")
+
+
+class PomodoroTimerApp(App):
+    """A textual Pomodoro Timer with audio playback support"""
+
+    timer: PomodoroTimer
+
+    def __init__(self, timer: PomodoroTimer, **kwargs):
+        super().__init__(**kwargs)
+        self.timer = timer
+
+    def on_mount(self) -> None:
+        self.timer.start()
+
+    def compose(self) -> ComposeResult:
+        yield Header()
+        yield TimerDisplay(timer=self.timer, id="clock")
+        yield Footer()
