e562a4b32f62a2d5f92e688268a3e0ad62013d60
Nikita Timoshenko <nikita.timoshenko.dev@gmail.com>
2026-07-08 22:35:44 +0300

    add app startup logic

--- tuipomodoro/__main__.py
+++ tuipomodoro/__main__.py
@@ -1,4 +1,11 @@
-def main(): ...
+from tuipomodoro.app import PomodoroTimerApp
+from tuipomodoro.timer import PomodoroTimer
+
+
+def main():
+    timer = PomodoroTimer(duration=(5 * 60))
+    app = PomodoroTimerApp(timer)
+    app.run()
 
 
 if __name__ == "__main__":
