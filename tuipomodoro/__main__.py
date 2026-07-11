4c4b25cb06880d6e07d565373b8e18aa538b392c
Nikita Timoshenko <nikita.timoshenko.dev@gmail.com>
2026-07-11 12:11:07 +0300

    add settings initialization from config.toml file

--- tuipomodoro/__main__.py
+++ tuipomodoro/__main__.py
@@ -1,10 +1,12 @@
 from tuipomodoro.app import PomodoroTimerApp
+from tuipomodoro.config import Settings
 from tuipomodoro.timer import PomodoroTimer
 
 
 def main():
-    timer = PomodoroTimer()
-    app = PomodoroTimerApp(timer)
+    settings = Settings.load()
+    timer = PomodoroTimer(settings.timer_duration)
+    app = PomodoroTimerApp(timer, settings)
     app.run()
 
 
